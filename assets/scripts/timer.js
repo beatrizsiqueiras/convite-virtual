88077804a89eb9f75abda3a5323755760c426deb
Beatriz Siqueira <88863619+beatrizsiq@users.noreply.github.com>
2022-11-11 19:55:57 -0300

    Update timer.js

--- assets/scripts/timer.js
+++ assets/scripts/timer.js
@@ -3,7 +3,7 @@ const timeElapsed = Date.now();
 let dataInicial = new Date(timeElapsed);
 dataInicial = dataInicial.toLocaleDateString();
 
-let dataFinal = new Date(2022, 9, 22);
+let dataFinal = new Date(2023, 8, 13);
 dataFinal = dataFinal.toLocaleDateString();
 
 var diff = moment(dataFinal,"DD/MM/YYYY").diff(moment(dataInicial,"DD/MM/YYYY"));
